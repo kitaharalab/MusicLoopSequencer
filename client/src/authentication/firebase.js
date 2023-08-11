8c9fa5c7ec685c70e9cd757315c8a985c2f61928
itochs <halglobe0108@gmail.com>
2023-08-11 15:54:51 +0900

    firebase app initialize

---
+++ client/src/authentication/firebase.js
@@ -0,0 +1,27 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_DEV_API_KEY,
+  authDomain: import.meta.env.VITE_DEV_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_DEV_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_DEV_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_DEV_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_DEV_APP_ID,
+  measurementId: import.meta.env.VITE_DEV_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// eslint-disable-next-line no-unused-vars
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+
+export default auth;
