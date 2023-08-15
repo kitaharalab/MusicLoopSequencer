8b661642617e42720d777d534735e04e6b25c934
itochs <halglobe0108@gmail.com>
2023-08-15 17:59:40 +0900

    chakra ui link as react router link

---
+++ client/components/Link/Link.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Link as ChakraLink } from "@chakra-ui/react";
+import { Link as ReactRouterLink } from "react-router-dom";
+
+// eslint-disable-next-line react/prop-types
+export default function Link({ children, ...res }) {
+  return (
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    <ChakraLink as={ReactRouterLink} {...res}>
+      {children}
+    </ChakraLink>
+  );
+}
