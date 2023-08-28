73b1a9f3687d25249355aea2704b4c87fe7258ab
itochs <halglobe0108@gmail.com>
2023-08-28 18:25:33 +0900

    見通しを良くした

--- client/src/SoundBlock.jsx
+++ client/src/SoundBlock.jsx
@@ -4,6 +4,64 @@ import { setPos } from "./redux/blockCanvasSlice";
 import { setMusicData } from "./redux/musicDataSlice";
 import selectBlock from "./selectBlock";
 
+function drawBackgroundOutline(canvas) {
+  const ctx = canvas.getContext("2d");
+  ctx.fillStyle = "black";
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = "3";
+  ctx.strokeRect(0, 0, canvas.width, canvas.height);
+  ctx.fill();
+}
+
+function drawBackgroundGrid(canvas) {
+  const ctx = canvas.getContext("2d");
+  ctx.lineWidth = "1";
+  ctx.strokeStyle = "gray";
+  for (let i = 0; i < 32; i++) {
+    for (let j = 0; j < 4; j++) {
+      ctx.strokeRect(i * 36, j * 50, 36, 50);
+    }
+  }
+  ctx.fill();
+}
+
+function drawBackground(canvas) {
+  const ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fill();
+  drawBackgroundGrid(canvas);
+  drawBackgroundOutline(canvas);
+}
+
+function drawSelectCell(canvas, measureId, partId) {
+  const ctx = canvas.getContext("2d");
+  ctx.lineWidth = 6;
+  ctx.strokeRect(36 * measureId, 50 * partId, 36, 50);
+  ctx.fill();
+}
+
+function drawPart(canvas, parts) {
+  const ctx = canvas.getContext("2d");
+
+  for (let i = 0; i < 4; i++) {
+    if (i === 0) {
+      ctx.fillStyle = "red";
+    } else if (i === 1) {
+      ctx.fillStyle = "yellow";
+    } else if (i === 2) {
+      ctx.fillStyle = "green";
+    } else {
+      ctx.fillStyle = "blue";
+    }
+    for (let j = 0; j < 32; j++) {
+      if (parts[i][j] != null) {
+        ctx.fillRect(j * 36, i * 50, 36, 50);
+      }
+    }
+  }
+  ctx.fill();
+}
+
 export default function SoundBlock() {
   // const selectedMeasureId = useSelector((state) => state.block.posRectX);
   // const selectedPartId = useSelector((state) => state.block.posRectY)
@@ -13,146 +71,23 @@ export default function SoundBlock() {
   const dispatch = useDispatch();
   const canvasRef = useRef();
 
-  // const clickRect = ({ nativeEvent }) => {
-  //    const { offsetX, offsetY } = nativeEvent;
-  //    dispatch(setPos({ offsetX, offsetY }))
-
-  // };
-
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-
-    ctx.clearRect(0, 0, 1152, 200);
-    ctx.strokeRect(0, 0, canvas.width, canvas.height);
-
-    if (parts.length !== 0) {
-      const sequenceList = parts[0].sounds;
-      const synthList = parts[1].sounds;
-      const bassList = parts[2].sounds;
-      const drumsList = parts[3].sounds;
-
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.strokeRect(0, 0, canvas.width, canvas.height);
-      for (let i = 0; i < 4; i++) {
-        if (i === 0) {
-          ctx.fillStyle = "red";
-        } else if (i === 1) {
-          ctx.fillStyle = "yellow";
-        } else if (i === 2) {
-          ctx.fillStyle = "green";
-        } else {
-          ctx.fillStyle = "blue";
-        }
-        for (let j = 0; j < 32; j++) {
-          if (i === 0) {
-            if (sequenceList[j] != null) {
-              ctx.fillRect(j * 36, i * 50, 36, 50);
-            }
-          } else if (i === 1) {
-            if (synthList[j] != null) {
-              ctx.fillRect(j * 36, i * 50, 36, 50);
-            }
-          } else if (i === 2) {
-            if (bassList[j] != null) {
-              ctx.fillRect(j * 36, i * 50, 36, 50);
-            }
-          } else if (drumsList[j] != null) {
-            ctx.fillRect(j * 36, i * 50, 36, 50);
-          }
-          ctx.fill();
-        }
-      }
-      for (let i = 0; i < 32; i++) {
-        for (let j = 0; j < 4; j++) {
-          ctx.strokeRect(i * 36, j * 50, 36, 50);
-        }
-      }
-      ctx.fill();
-    }
-
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 1;
-    for (let i = 0; i < 32; i++) {
-      for (let j = 0; j < 4; j++) {
-        ctx.strokeRect(36 * i, 50 * j, 36, 50);
-      }
-    }
-    ctx.lineWidth = 6;
-    ctx.strokeRect(36 * measureId, 50 * partId, 36, 50);
-    ctx.fill();
-    ctx.lineWidth = 1;
-    for (let i = 0; i < 32; i++) {
-      for (let j = 0; j < 4; j++) {
-        ctx.strokeRect(36 * i, 50 * j, 36, 50);
-      }
-    }
+    drawBackground(canvasRef.current);
+    drawSelectCell(canvasRef.current, measureId, partId);
   }, [measureId, partId]);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeRect(0, 0, canvas.width, canvas.height);
-
-    for (let i = 0; i < 32; i++) {
-      for (let j = 0; j < 4; j++) {
-        ctx.strokeRect(i * 36, j * 50, 36, 50);
-      }
-    }
-    ctx.fill();
+    drawBackground(canvasRef.current);
   }, []);
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-
     if (parts.length === 0) {
       return;
     }
-    const sequenceList = parts[0].sounds;
-    const synthList = parts[1].sounds;
-    const bassList = parts[2].sounds;
-    const drumsList = parts[3].sounds;
 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < 4; i++) {
-      if (i === 0) {
-        ctx.fillStyle = "red";
-      } else if (i === 1) {
-        ctx.fillStyle = "yellow";
-      } else if (i === 2) {
-        ctx.fillStyle = "green";
-      } else {
-        ctx.fillStyle = "blue";
-      }
-      for (let j = 0; j < 32; j++) {
-        if (i === 0) {
-          if (sequenceList[j] != null) {
-            ctx.fillRect(j * 36, i * 50, 36, 50);
-          }
-        } else if (i === 1) {
-          if (synthList[j] != null) {
-            ctx.fillRect(j * 36, i * 50, 36, 50);
-          }
-        } else if (i === 2) {
-          if (bassList[j] != null) {
-            ctx.fillRect(j * 36, i * 50, 36, 50);
-          }
-        } else if (drumsList[j] != null) {
-          ctx.fillRect(j * 36, i * 50, 36, 50);
-        }
-        ctx.fill();
-      }
-    }
-    for (let i = 0; i < 32; i++) {
-      for (let j = 0; j < 4; j++) {
-        ctx.strokeRect(i * 36, j * 50, 36, 50);
-      }
-    }
-    ctx.fill();
+    const canvas = canvasRef.current;
+    drawBackground(canvas);
+    drawPart(canvas, parts);
   }, [parts]);
 
   return (
