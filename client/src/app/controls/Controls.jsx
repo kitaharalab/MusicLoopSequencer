71fef6f7b12eff5dd94d7595774394c2c962910e
itochs <halglobe0108@gmail.com>
2023-09-11 15:50:22 +0900

    評価ボタンの追加

--- client/src/app/controls/Controls.jsx
+++ client/src/app/controls/Controls.jsx
@@ -1,32 +1,36 @@
-import React, { useState } from "react";
-import { FormControl, Button, Box } from "@chakra-ui/react";
+import React from "react";
+import { FormControl, Button, Box, Flex, Spacer } from "@chakra-ui/react";
 import { useDispatch, useSelector } from "react-redux";
 import createMusic from "../../createMusic";
 import { setParts } from "../../redux/soundsSlice";
 import { setId } from "../../redux/songIdSlice";
 import AudioControls from "./AudioControls";
+import Evaluation from "./Evaluation";
 
 export default function Controls({ projectId }) {
   const dispatch = useDispatch();
   const linesY = useSelector((state) => state.lines1.lines);
 
   return (
-    <FormControl display="flex">
-      <Box>
-        <Button
-          type="button"
-          onClick={async () => {
-            const music = await createMusic(projectId, linesY);
-            dispatch(setParts(music.parts));
-            dispatch(setId(music.songid));
-          }}
-        >
-          create
-        </Button>
-      </Box>
-      <Box>
+    <FormControl>
+      <Flex>
+        <Box>
+          <Button
+            type="button"
+            onClick={async () => {
+              const music = await createMusic(projectId, linesY);
+              dispatch(setParts(music.parts));
+              dispatch(setId(music.songid));
+            }}
+          >
+            create
+          </Button>
+        </Box>
+        <Spacer />
         <AudioControls />
-      </Box>
+        <Spacer />
+        <Evaluation />
+      </Flex>
     </FormControl>
   );
 }
