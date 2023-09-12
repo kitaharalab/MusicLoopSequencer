9b280f710b311d032731fca64f1e82938922210d
itochs <halglobe0108@gmail.com>
2023-09-12 15:41:32 +0900

    sample topic

--- client/src/app/musicEdit/TopicView.jsx
+++ client/src/app/musicEdit/TopicView.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 
+const sampleMusicTopic = [
+  { name: "lowest", value: 10 },
+  { name: "lower", value: 10 },
+  { name: "higher", value: 10 },
+  { name: "highest", value: 10 },
+];
+
+const sampleLoopTopic = [
+  { name: "lowest", value: 1 },
+  { name: "lower", value: 2 },
+  { name: "higher", value: 3 },
+  { name: "highest", value: 4 },
+];
+
 export default function TopicView() {
   return <div>topic</div>;
 }
