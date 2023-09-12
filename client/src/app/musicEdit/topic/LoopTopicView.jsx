c6e0cbb7846cfa2330ebc034f2d4085a729e268a
itochs <halglobe0108@gmail.com>
2023-09-12 17:31:03 +0900

    棒グラフ部分の切り出し

--- client/src/app/musicEdit/topic/LoopTopicView.jsx
+++ client/src/app/musicEdit/topic/LoopTopicView.jsx
@@ -1,35 +1,7 @@
+/* eslint-disable import/no-unresolved */
 import React from "react";
-import * as d3 from "d3";
+import BarChart from "@components/chart/BarChart";
 
 export default function LoopTopicView({ data, width, height }) {
-  const musicXScale = d3
-    .scaleBand()
-    .domain(data.map(({ name }) => name))
-    .padding(0.2)
-    .range([0, width]);
-  const musicYScale = d3
-    .scaleLinear()
-    .domain([0, d3.max(data.map(({ value }) => value))])
-    .range([height, 0]);
-  const colorScale = d3
-    .scaleSequential(d3.interpolateRainbow)
-    .domain([0, data.length]);
-  return (
-    <svg width={width} height={height}>
-      <g>
-        {data.map(({ name, value }, i) => {
-          console.log(name);
-          return (
-            <rect
-              x={musicXScale(name)}
-              y={musicYScale(value)}
-              width={musicXScale.bandwidth()}
-              height={height - musicYScale(value)}
-              fill={colorScale(i)}
-            />
-          );
-        })}
-      </g>
-    </svg>
-  );
+  return <BarChart data={data} width={width} height={height} />;
 }
