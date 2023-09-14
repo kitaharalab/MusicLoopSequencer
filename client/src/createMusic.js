bca151bf39fd36581cd3c7de484a8673783c8513
itochs <halglobe0108@gmail.com>
2023-09-14 18:45:37 +0900

    最大値から盛り上がり度を5段階に分割

--- client/src/createMusic.js
+++ client/src/createMusic.js
@@ -1,20 +1,17 @@
 import axios from "axios";
 
-export default function createMusic(projectid, linesY) {
+export default function createMusic(projectid, linesY, max) {
   const excitementArray = new Array(32);
-  for (let i = 0; i < 32; i++) {
-    // １ブロックの範囲を決定
-    const blockStart = Math.floor(i * 36);
-    const blockFinish = Math.floor(blockStart + 36);
-    // ブロックの合計から平均を計算
-    let blockTotal = 0;
-    for (let j = blockStart; j < blockFinish; j++) {
-      blockTotal += Math.abs(linesY[j]);
-
-      excitementArray[i] = Math.floor(blockTotal / 36 / 56);
-    }
+  const range = Math.floor(linesY.length / excitementArray.length);
+  const excitementStep = 5;
+  console.log(range);
+  for (let i = 0; i < excitementArray.length; i++) {
+    const sliceExcitementValues = linesY.slice(i * range, (i + 1) * range);
+    const total = sliceExcitementValues.reduce((sum, value) => sum + value);
+    excitementArray[i] = Math.floor(
+      (total / (max * (range + 1))) * excitementStep,
+    );
   }
-  console.log("button pushed");
   const url = `${import.meta.env.VITE_SERVER_URL}/projects/${String(
     projectid,
   )}/songs`;
