d7cbbf663954530051fb2d1834b71f7f83d744c9
itochs <halglobe0108@gmail.com>
2023-09-21 00:00:58 +0900

    サンプルデータという表示

--- client/src/app/musicEdit/topic/TopicPreferenceView.jsx
+++ client/src/app/musicEdit/topic/TopicPreferenceView.jsx
@@ -11,6 +11,17 @@ export default function TopicPreferenceView({ data, width, height, padding }) {
         height={height}
         padding={padding}
       />
+      <rect
+        x={0}
+        y={0}
+        width={width}
+        height={height}
+        fill="white"
+        opacity={0.7}
+      />
+      <text x={width / 2} y={height / 2} textAnchor="middle" fontSize={50}>
+        Sample
+      </text>
       <line x1={0} y1={height} x2={width} y2={height} stroke="black" />
     </g>
   );
