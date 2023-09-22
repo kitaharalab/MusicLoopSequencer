ae5be0cc223da487b15a07bf8118690271ea9ad2
itochs <halglobe0108@gmail.com>
2023-09-22 17:03:20 +0900

    選択したときのハイライト

--- client/src/app/musicEdit/loopMaterisl/ScatterPlot.jsx
+++ client/src/app/musicEdit/loopMaterisl/ScatterPlot.jsx
@@ -1,11 +1,13 @@
 import * as d3 from "d3";
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
-export default function ScatterPlot({ width, height }) {
+export default function ScatterPlot({ width, height, selectState }) {
   const xCoordinate = useSelector((state) => state.musicData.xCoordinate);
   const yCoordinate = useSelector((state) => state.musicData.yCoordinate);
   const rangeList = useSelector((state) => state.musicData.rangeList);
+  const [selectId, setSelectId] = useState();
+
   if (!xCoordinate || !yCoordinate || !rangeList || !width || !height) {
     return <g />;
   }
@@ -29,14 +31,23 @@ export default function ScatterPlot({ width, height }) {
 
   return (
     <g>
-      {loopMaterials.map(({ x, y, id }, i) => (
-        <circle
-          key={id}
-          transform={`translate(${xScale(x)} ${yScale(y)})`}
-          r={3}
-          fill={colorScale(rangeList.findLastIndex((range) => i > range))}
-        />
-      ))}
+      {loopMaterials.map(({ x, y, id }, i) => {
+        const fillColor = colorScale(
+          rangeList.findLastIndex((range) => i > range),
+        );
+        return (
+          <circle
+            key={id}
+            transform={`translate(${xScale(x)} ${yScale(y)})`}
+            r={selectId === id ? 4 : 3}
+            fill={fillColor}
+            fillOpacity={selectId === undefined || selectId === id ? 1 : 0.5}
+            onClick={() => {
+              setSelectId(selectId === id ? undefined : id);
+            }}
+          />
+        );
+      })}
     </g>
   );
 }
