3836eff1bf67da14633cde58769c9a77ce838e57
itochs <halglobe0108@gmail.com>
2023-10-25 03:26:51 +0900

    import

--- server/app/sqls/__init__.py
+++ server/app/sqls/__init__.py
@@ -1,7 +1,7 @@
 from .connection import get_connection
 from .part import get_part_name, get_parts
-from .project import add_project, get_projects
-from .song import add_song, sound_array_wrap
+from .project import add_project, get_project_song_ids, get_projects
+from .song import add_song, get_excitement_curve, sound_array_wrap
 from .song_details import get_song_details, update_song_details
 
 __all__ = [
