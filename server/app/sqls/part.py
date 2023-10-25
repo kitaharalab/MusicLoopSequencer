cd8a37d0c1206be4473849b8c804e650471c228d
itochs <halglobe0108@gmail.com>
2023-10-25 21:59:13 +0900

    カラム名のミス

--- server/app/sqls/part.py
+++ server/app/sqls/part.py
@@ -20,7 +20,7 @@ def get_part_name(part_id):
     response = None
     with get_connection() as conn:
         with conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute("SELECT part_name FROM parts WHERE id = %s", (part_id,))
+            cur.execute("SELECT name FROM parts WHERE id = %s", (part_id,))
             result = dict(cur.fetchone())
             response: str = result["name"]
 
