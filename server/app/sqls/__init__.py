8e7b3fc48361e14c012e49d90fdbf32858087344
itochs <halglobe0108@gmail.com>
2023-10-27 02:34:58 +0900

    関数名とやってることが違ったので修正

--- server/app/sqls/__init__.py
+++ server/app/sqls/__init__.py
@@ -3,7 +3,7 @@ from .log import play_loop_log, play_song_log
 from .part import get_part_name, get_parts
 from .project import add_project, get_project, get_project_song_ids, get_projects
 from .song import create_song, get_excitement_curve, sound_array_wrap
-from .song_details import get_song_details, update_song_details
+from .song_details import get_song_details, get_song_loop_ids, update_song_details
 
 __all__ = [
     "get_connection",
@@ -12,6 +12,6 @@ __all__ = [
     "get_projects",
     "create_song",
     "sound_array_wrap",
-    "get_song_details",
+    "get_song_loop_ids",
     "update_song_details",
 ]
