6bc160f7d3d856c574081250610be8703e886278
itochs <halglobe0108@gmail.com>
2023-10-27 01:08:40 +0900

    休憩中の表示

--- client/src/experiment/project/components/RestButton.jsx
+++ client/src/experiment/project/components/RestButton.jsx
@@ -1,6 +1,43 @@
-import { Button } from "@chakra-ui/react";
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import React from "react";
 
 export default function RestButton() {
-  return <Button>休憩する</Button>;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  function handleClose() {
+    onClose();
+  }
+
+  return (
+    <>
+      <Button onClick={onOpen}>休憩する</Button>
+      <Modal
+        closeOnOverlayClick={false}
+        isOpen={isOpen}
+        onClose={handleClose}
+        isCentered
+      >
+        <ModalOverlay background="blackAlpha.300" backdropFilter="blur(8px)" />
+
+        <ModalContent>
+          <ModalHeader>休憩中</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody />
+          <ModalFooter>
+            <Button onClick={handleClose}>作業に戻る</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
 }
