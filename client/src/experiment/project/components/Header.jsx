c203c8a829e8ee37199b668b6da13b340444a397
itochs <halglobe0108@gmail.com>
2023-10-27 00:55:48 +0900

    休憩ボタンを別ファイルに移動

--- client/src/experiment/project/components/Header.jsx
+++ client/src/experiment/project/components/Header.jsx
@@ -1,13 +1,15 @@
-import { Heading, Text, Button, Center, Box } from "@chakra-ui/react";
+import { Heading, Text, Center, Box } from "@chakra-ui/react";
 import React from "react";
+
+import RestButton from "./RestButton";
 
 export default function Header({ projectName }) {
   return (
     <Heading as="h1" marginY={4}>
       <Box position="relative">
-        <Button position="absolute" top={1}>
-          休憩する
-        </Button>
+        <Box position="absolute">
+          <RestButton />
+        </Box>
       </Box>
 
       <Center>
---
+++ client/src/experiment/project/components/RestButton.jsx
@@ -0,0 +1,6 @@
+import { Button } from "@chakra-ui/react";
+import React from "react";
+
+export default function RestButton() {
+  return <Button>休憩する</Button>;
+}
