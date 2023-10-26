f67fb4c1a2fad16a340d75847b4a0977d1b873fd
itochs <halglobe0108@gmail.com>
2023-10-27 00:15:25 +0900

    実験用のページで新しくプロジェクトを作れないようにした

--- client/src/experiment/Projects.jsx
+++ client/src/experiment/Projects.jsx
@@ -18,45 +18,21 @@ import Link from "../../components/Link/Link";
 export default function Projects() {
   const [sample, _setSample] = useState(null);
   const [projects, setProjects] = useState([]);
-  const navigate = useNavigate();
-
-  const createNewProject = () => {
-    const url = `${import.meta.env.VITE_SERVER_URL}/projects`;
-    axios
-      .post(url) // サーバーから音素材の配列を受け取った後，then部分を実行する．
-      .then((response) => {
-        const { data } = response;
-        setProjects([...projects, data]);
-      });
-  };
 
   useEffect(() => {
-    axios
-      .get(`${import.meta.env.VITE_SERVER_URL}/projects`)
-      .then((response) => {
-        const { data } = response;
-        setProjects(data);
-      });
+    const baseURL = new URL(`${import.meta.env.VITE_SERVER_URL}/projects`);
+    const url = new URL(`?experiment=true`, baseURL);
+    axios.get(url).then((response) => {
+      const { data } = response;
+      setProjects(data);
+    });
   }, [sample]);
 
   return (
     <Box id="project">
       <Card bgColor="darkslategrey" align="center" width="30vw">
         <CardBody>
-          <Box>
-            <IconButton
-              type="button"
-              onClick={() => {
-                createNewProject();
-                const createdProject = projects[projects.length - 1];
-                navigate(`${createdProject.id}`);
-              }}
-              icon={<AddIcon />}
-              width="25%"
-              alignSelf="center"
-            />
-          </Box>
-          <Text color="white">create new project</Text>
+          <Text color="white">Projects</Text>
         </CardBody>
       </Card>
       <SimpleGrid minChildWidth="30vw" spacing={4} marginTop={2}>
