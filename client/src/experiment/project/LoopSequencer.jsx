f0bdea5ca52f4579102f1b20450482ef6be1679d
itochs <halglobe0108@gmail.com>
2023-10-27 00:38:01 +0900

    プロジェクトの名前の取得

--- client/src/experiment/project/LoopSequencer.jsx
+++ client/src/experiment/project/LoopSequencer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import Content from "./components/Content";
@@ -6,9 +6,23 @@ import Header from "./components/Header";
 
 export default function LoopSequencer() {
   const { projectId } = useParams();
+  const [projectName, setProjectName] = useState();
+
+  async function getProjectName() {
+    const url = `${import.meta.env.VITE_SERVER_URL}/projects/${projectId}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const {
+      project: { name },
+    } = data;
+    setProjectName(name);
+  }
+
+  getProjectName();
+
   return (
     <>
-      <Header projectName={projectId} />
+      <Header projectName={projectName} />
       <Content projectId={projectId} />
     </>
   );
