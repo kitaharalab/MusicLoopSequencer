758d35cb33b903ee1dd42e7bad5f5b96ffd040ac
itochs <halglobe0108@gmail.com>
2023-11-03 03:01:31 +0900

    Add Firebase authentication to Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,22 +1,38 @@
 import { Box, Divider, Flex, Heading, Spacer } from "@chakra-ui/react";
-import React from "react";
+import { onAuthStateChanged, getAuth } from "firebase/auth";
+import React, { useEffect, useState } from "react";
 
 import Link from "../../components/Link/Link";
 
 export default function Header() {
+  const auth = getAuth();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (authUser) => {
+      setUser(authUser);
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <Heading>
       <Flex>
         Music Loop Sequencer
         <Spacer />
-        <Box>
-          <Link to="/signin" padding={3}>
-            Sign in
-          </Link>
-          <Link to="/signup" padding={3}>
-            Sign up
-          </Link>
-        </Box>
+        {user === null || user === undefined ? (
+          <Box>
+            <Link to="/signin" padding={3}>
+              Sign in
+            </Link>
+            <Link to="/signup" padding={3}>
+              Sign up
+            </Link>
+          </Box>
+        ) : (
+          <div>signout</div>
+        )}
       </Flex>
       <Divider />
     </Heading>
