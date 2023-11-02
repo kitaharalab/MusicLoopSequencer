20120e45607a3572d6f63eb83944b7c3178b042e
itochs <halglobe0108@gmail.com>
2023-11-03 02:41:12 +0900

    Add Firebase authentication and display user email in Project component

--- client/src/Project.jsx
+++ client/src/Project.jsx
@@ -12,17 +12,20 @@ import {
   Spacer,
 } from "@chakra-ui/react";
 import axios from "axios";
+import { getAuth } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 
 import Link from "../components/Link/Link";
 
-import "./Project.css";
-import SignUp from "./authentication/SignUp";
+import initializeFirebase from "./authentication/firebase";
+
+initializeFirebase();
 
 function Project() {
   const [done, setDone] = useState(false);
   const [sample, _setSample] = useState(null);
   const [projects, setProjects] = useState([]);
+  const auth = getAuth();
 
   const createNewProject = () => {
     const url = `${import.meta.env.VITE_SERVER_URL}/projects`;
@@ -59,7 +62,7 @@ function Project() {
 
   return (
     <Box id="project">
-      <SignUp />
+      {JSON.stringify(auth.currentUser.email)}
       <Flex>
         <Card bgColor="darkslategrey" align="center" width="30vw">
           <CardBody>
