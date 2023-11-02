efdd54c59a99e89463fb4d680e99781767b5cd8f
itochs <halglobe0108@gmail.com>
2023-11-03 01:12:47 +0900

    Refactor Firebase initialization code

--- client/src/authentication/firebase.js
+++ client/src/authentication/firebase.js
@@ -1,13 +1,7 @@
-/* eslint-disable import/no-extraneous-dependencies */
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_DEV_API_KEY,
   authDomain: import.meta.env.VITE_DEV_AUTH_DOMAIN,
@@ -18,10 +12,12 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_DEV_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// eslint-disable-next-line no-unused-vars
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
+function initializeFirebase() {
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  // eslint-disable-next-line no-unused-vars
+  const analytics = getAnalytics(app);
+  const auth = getAuth(app);
+}
 
-export default auth;
+export default initializeFirebase;
