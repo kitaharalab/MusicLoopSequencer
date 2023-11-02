669bb86be698c684d73a15e1b7577cce10836399
itochs <halglobe0108@gmail.com>
2023-11-03 02:40:34 +0900

    Refactor SignUp component with Chakra UI and Firebase authentication

--- client/src/authentication/SignUp.jsx
+++ client/src/authentication/SignUp.jsx
@@ -1,13 +1,30 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import React, { useEffect, useState } from "react";
+import {
+  Container,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Button,
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+} from "@chakra-ui/react";
 import {
   createUserWithEmailAndPassword,
   onAuthStateChanged,
+  signInWithEmailAndPassword,
+  getAuth,
 } from "firebase/auth";
-import auth from "./firebase";
+import React, { useEffect, useState } from "react";
+import { flushSync } from "react-dom";
+import { Navigate } from "react-router-dom";
 
 export default function SignUp() {
-  const [user, setUser] = useState(null);
+  const auth = getAuth();
+  const [user, setUser] = useState();
+  const [creating, setCreating] = useState(false);
 
   useEffect(() => {
     onAuthStateChanged(auth, (authUser) => {
@@ -15,58 +32,86 @@ export default function SignUp() {
     });
   }, []);
 
+  async function handleSubmit(event) {
+    event.preventDefault();
+    flushSync(() => {
+      setCreating(true);
+    });
+    const { email, password } = event.target.elements;
+    try {
+      await createUserWithEmailAndPassword(auth, email.value, password.value);
+    } catch (error) {
+      console.log(error);
+      signInWithEmailAndPassword(auth, email.value, password.value).then(
+        (userCredential) => {
+          const { user } = userCredential;
+          setUser(user);
+        },
+      );
+    }
+
+    setCreating(false);
+  }
+
+  if (user !== null) {
+    return (
+      // <div>
+      //   {user !== null && <Navigate to="/" />}
+      //   already sign in
+      //   <div>
+      //     <button
+      //       type="button"
+      //       onClick={() => {
+      //         signOut(auth).then(() => {
+      //           console.log("sign out");
+      //         });
+      //       }}
+      //     >
+      //       sign out
+      //     </button>
+      //   </div>
+      // </div>
+      <Navigate to="/test" />
+    );
+  }
+
   return (
-    <div>
-      <p>sign up</p>
-      <p>現在のユーザーのemail：{user?.email ?? "none"}</p>
-      <p>ユーザー登録</p>
-      <form
-        onSubmit={async (e) => {
-          e.preventDefault();
-          const { email, password } = e.target.elements;
-          try {
-            await createUserWithEmailAndPassword(
-              auth,
-              email.value,
-              password.value,
-            );
-          } catch (error) {
-            console.log(error);
-          }
-        }}
-      >
-        <div>
-          <label htmlFor="signup-email">
-            mail
-            <input
-              id="signup-email"
-              name="email"
-              type="email"
-              placeholder="email"
-              autoComplete="username"
-              disabled={user}
-            />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="signup-email">
-            password
-            <input
-              id="signup-email"
-              name="password"
-              type="password"
-              placeholder="password"
-              autoComplete="new-password"
-              disabled={user}
-            />
-          </label>
-        </div>
-        <div>
-          <button type="submit" disabled={user}>
-            send
-          </button>
-        </div>
-      </form>
-    </div>
+    <Container
+      centerContent
+      justifyContent="center"
+      height="100vh"
+      minWidth="50%"
+    >
+      <Card width="100%">
+        <CardHeader>
+          <Heading textAlign="center">ユーザー登録</Heading>
+        </CardHeader>
+        <CardBody>
+          <form onSubmit={handleSubmit}>
+            <FormControl isRequired>
+              <FormLabel htmlFor="signup-email">email</FormLabel>
+              <Input
+                id="signup-email"
+                name="email"
+                type="email"
+                autoComplete="username"
+              />
+              <FormLabel htmlFor="signup-password">password</FormLabel>
+              <Input
+                id="signup-password"
+                name="password"
+                type="password"
+                autoComplete="new-password"
+              />
+            </FormControl>
+            <Box marginTop={3}>
+              <Button type="submit" isLoading={creating} width="100%">
+                create
+              </Button>
+            </Box>
+          </form>
+        </CardBody>
+      </Card>
+    </Container>
   );
 }
