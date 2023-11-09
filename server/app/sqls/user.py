1fa30ba0600b00ebca70fe77cec4f1c27f1070a8
itochs <halglobe0108@gmail.com>
2023-11-09 23:41:45 +0900

    userIDだけ保持するようにしたので修正．

--- server/app/sqls/user.py
+++ server/app/sqls/user.py
@@ -3,9 +3,18 @@ from psycopg2.extras import DictCursor
 from .connection import get_connection
 
 
-def add_user(user_id: str, email: str):
-    create_sql = "INSERT INTO users (user_id, email) VALUES (%s, %s)"
+def add_user(user_id: str):
+    create_sql = "INSERT INTO users (user_id) VALUES (%s)"
     with get_connection() as conn:
         with conn.cursor(cursor_factory=DictCursor) as cur:
-            cur.execute(create_sql, (user_id, email))
+            cur.execute(create_sql, (user_id,))
             conn.commit()
+
+
+def get_user(user_id: str):
+    select_sql = "SELECT * FROM users WHERE user_id = %s"
+    with get_connection() as conn:
+        with conn.cursor(cursor_factory=DictCursor) as cur:
+            cur.execute(select_sql, (user_id,))
+            result = cur.fetchone()
+            return dict(result) if result is not None else None
