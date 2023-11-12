a3a9f813ef41d541c5fb2232ef351f8593801ff7
itochs <halglobe0108@gmail.com>
2023-11-13 00:03:16 +0900

    ALTER TABLEで変更してデータを入れてからからテーブルの定義に付け足した

--- server/sql/create_loops_table.sql
+++ server/sql/create_loops_table.sql
@@ -1,9 +1,15 @@
-CREATE TABLE
+CREATE TABLE IF NOT EXISTS
     loops (
         id serial PRIMARY KEY,
         NAME TEXT NOT NULL,
         part_id INTEGER NOT NULL,
         excitement INTEGER NOT NULL,
         DATA bytea NOT NULL,
-        FOREIGN KEY (part_id) REFERENCES parts (id)
+        x DOUBLE PRECISION,
+        y DOUBLE PRECISION FOREIGN KEY (part_id) REFERENCES parts (id)
     );
+
+-- ALTER TABLE loops
+-- ADD IF NOT EXISTS x DOUBLE PRECISION;
+-- ALTER TABLE loops
+-- ADD IF NOT EXISTS y DOUBLE PRECISION;
