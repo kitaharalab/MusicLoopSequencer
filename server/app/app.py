8070f1c83952c52da708bd37d585678119fd255b
itochs <halglobe0108@gmail.com>
2023-11-16 18:06:51 +0900

    send_fileで送信

--- server/app/app.py
+++ server/app/app.py
@@ -804,10 +804,11 @@ def log_play_song(uid, projectid, songid):
 @app.route("/parts/<int:partid>/musicloops/<musicloopid>/wav", methods=["GET"])
 def download_musicloop(partid, musicloopid):
     data = get_loop_music_by_id(musicloopid)
-    response = make_response(data)
-    response.headers.set("Content-Type", request.content_type)
 
-    return response
+    if data is None:
+        return make_response(jsonify({"message": "指定された楽曲ファイルは存在しません"})), 204
+
+    return send_file(io.BytesIO(data), mimetype="audio/wav")
 
 
 @app.route("/parts/<int:partid>/musicloops/<musicloopid>/wav", methods=["POST"])
