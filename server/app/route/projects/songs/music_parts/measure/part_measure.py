eb3e161e3668e354f6801812124d937f60240e8c
itochs <halglobe0108@gmail.com>
2023-11-16 21:05:03 +0900

    デリート実装用のコメント

--- server/app/route/projects/songs/music_parts/measure/part_measure.py
+++ server/app/route/projects/songs/music_parts/measure/part_measure.py
@@ -42,3 +42,141 @@ def get_infomation_of_inserted_sound(projectid, songid, partid, measureid):
     }
 
     return make_response(jsonify(response))
+
+
+# TODO: デリートの実装
+# @app.route(
+#     "/projects/<projectid>/songs/<songid>/parts/<partid>/measures/<measureid>",
+#     methods=["DELETE"],
+# )
+# @require_auth
+# def delete_sound(projectid, songid, partid, measureid):
+#     req = request.args
+#     fix = req.get("fix")
+#     sound_array = load_music_data(
+#         "./project/" + projectid + "/songs/" + songid + "/song" + songid + ".txt"
+#     )
+
+#     drums_list, bass_list, synth_list, sequence_list = get_sound_data()
+
+#     sound_array = delete_music_loop(
+#         measureid,
+#         partid,
+#         sound_array,
+#         drums_list,
+#         bass_list,
+#         synth_list,
+#         sequence_list,
+#         fix,
+#     )
+#     # root = tk.Tk()
+#     # view = View(master=root)
+#     songid = connect_sound(sound_array, projectid, "delete", songid)
+
+#     save_music_data(
+#         projectid, songid, sound_array, drums_list, bass_list, synth_list, sequence_list
+#     )
+
+#     drums_list, bass_list, synth_list, sequence_list = format_list(sound_array)
+
+#     response = {
+#         "songid": int(songid),
+#         "parts": [
+#             {"partid": 0, "sounds": sequence_list},
+#             {"partid": 1, "sounds": synth_list},
+#             {"partid": 2, "sounds": bass_list},
+#             {"partid": 3, "sounds": drums_list},
+#         ],
+#     }
+#     return make_response(jsonify(response))
+
+
+# def delete_music_loop(
+#     measureid,
+#     partid,
+#     sound_array,
+#     drums_list,
+#     bass_list,
+#     synth_list,
+#     sequence_list,
+#     fix,
+# ):
+#     if fix == 0:
+#         sound_array[int(measureid)][3 - int(partid)] = None
+#     else:
+#         for i in range(4):
+#             sound_array[int(int(measureid) / 4) * 4 + i][3 - int(partid)] = None
+
+#     for i in range(len(sound_array)):
+#         for j in range(len(sound_array[0])):
+#             if j == 0:
+#                 for k in range(len(drums_list)):
+#                     if sound_array[i][j] == k:
+#                         sound_array[i][j] = drums_list[k]
+#                     elif sound_array[i][j] == None:
+#                         sound_array[i][j] = "null"
+#             elif j == 1:
+#                 for k in range(len(bass_list)):
+#                     if sound_array[i][j] == k:
+#                         sound_array[i][j] = bass_list[k]
+#                     elif sound_array[i][j] == None:
+#                         sound_array[i][j] = "null"
+#             elif j == 2:
+#                 for k in range(len(synth_list)):
+#                     if sound_array[i][j] == k:
+#                         sound_array[i][j] = synth_list[k]
+#                     elif sound_array[i][j] == None:
+#                         sound_array[i][j] = "null"
+#             else:
+#                 for k in range(len(sequence_list)):
+#                     if sound_array[i][j] == k:
+#                         sound_array[i][j] = sequence_list[k]
+#                     elif sound_array[i][j] == None:
+#                         sound_array[i][j] = "null"
+#     return sound_array
+
+# def load_music_data(path):
+#     data_list = read_file(path)
+
+#     measure_number = 0
+#     sound_array = [["null" for i in range(4)] for j in range(32)]
+
+#     for i in range(len(data_list)):
+#         if data_list[i] != "null":
+#             sound_array[measure_number][i % 4] = int(data_list[i])
+#         else:
+#             sound_array[measure_number][i % 4] = None
+#         if i % 4 == 3:
+#             measure_number += 1
+
+
+# def save_music_data(
+#     projectid,
+#     songid,
+#     sound_array,
+#     drums_list,
+#     bass_list,
+#     synth_list,
+#     sequence_list,
+#     user_id,
+# ):
+#     for i in range(len(sound_array)):
+#         for j in range(len(sound_array[0])):
+#             if j == 0:
+#                 for k in range(len(drums_list)):
+#                     if sound_array[i][j] == drums_list[k]:
+#                         sound_array[i][j] = str(k)
+#             elif j == 1:
+#                 for k in range(len(bass_list)):
+#                     if sound_array[i][j] == bass_list[k]:
+#                         sound_array[i][j] = str(k)
+#             elif j == 2:
+#                 for k in range(len(synth_list)):
+#                     if sound_array[i][j] == synth_list[k]:
+#                         sound_array[i][j] = str(k)
+#             else:
+#                 for k in range(len(sequence_list)):
+#                     if sound_array[i][j] == sequence_list[k]:
+#                         sound_array[i][j] = str(k)
+
+#     add_song(sound_array_wrap(sound_array), songid, user_id)
