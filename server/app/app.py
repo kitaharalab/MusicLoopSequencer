af03c5094d17bd4130734e0b061d6c8079fa2131
itochs <halglobe0108@gmail.com>
2023-11-16 20:31:34 +0900

    曲の楽器詳細

--- server/app/app.py
+++ server/app/app.py
@@ -13,6 +13,7 @@ from route.parts.id.sounds import sounds
 from route.parts.id.sounds.id import sound_id
 from route.projects import projects
 from route.projects.songs import songs
+from route.projects.songs.music_parts import song_parts
 from sqls import create_song as add_song
 from sqls import (
     get_loop_music_by_id,
@@ -49,42 +50,7 @@ app.register_blueprint(sounds, url_prefix="/parts/<int:partid>/sounds")
 # app.register_blueprint(sound_id, url_prefix="/parts/<int:partid>/sounds/<int:soundid>")
 app.register_blueprint(projects)
 app.register_blueprint(songs)
-
-
-
-
-# TODO: どこで使ってるか不明
-@app.route("/projects/<projectid>/songs/<songid>/parts/<partid>", methods=["GET"])
-def get_infomation_of_inserted_sounds_in_selected_part(projectid, songid, partid):
-    sounds_ids = [["null" for i in range(4)] for j in range(32)]
-    id_list = []
-    path = "./project/" + projectid + "/songs/" + songid + "/song" + songid + ".txt"
-    with open(path) as f:
-        id_list = f.read().split("\n")
-    if id_list[len(id_list) - 1] == "":
-        id_list.pop()
-    count = 0
-
-    for i in range(len(id_list)):
-        sounds_ids[count][i % 4] = id_list[i]
-        if i % 4 == 3:
-            count = count + 1
-    partid_list = ["null" for i in range(32)]
-
-    for i in range(32):
-        if partid == "0":
-            partid_list[i] = sounds_ids[i][3]
-        elif partid == "1":
-            partid_list[i] = sounds_ids[i][2]
-        elif partid == "2":
-            partid_list[i] = sounds_ids[i][1]
-        else:
-            partid_list[i] = sounds_ids[i][0]
-
-    # response = {"sounds_ids": sounds_ids}
-    response = {"partid": partid, "sounds": partid_list}
-
-    return make_response(jsonify(response))
+app.register_blueprint(song_parts)
 
 
 @app.route(
