1e059f7b12dc58ed09b6b52644a53211dbab7df0
itochs <halglobe0108@gmail.com>
2023-11-16 18:01:56 +0900

    データの送信をsend_fileで送るように変更

--- server/app/app.py
+++ server/app/app.py
@@ -774,9 +774,16 @@ def downloadSong(projectid,songid,filename):
 @app.route("/projects/<projectid>/songs/<songid>/wav", methods=["GET"])
 def download_song(projectid, songid):
     data = get_wav_data_from_song_id(songid)
-    response = make_response(data)
-    response.headers.set("Content-Type", request.content_type)
-    return response
+
+    if data is None:
+        return make_response(jsonify({"message": "指定された楽曲ファイルは存在しません"})), 204
+
+    return send_file(
+        io.BytesIO(data),
+        mimetype="audio/wav",
+        as_attachment=True,
+        download_name=f"song_{songid}.wav",
+    )
 
 
 @app.route("/projects/<projectid>/songs/<songid>/wav", methods=["POST"])
