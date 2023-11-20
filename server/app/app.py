95b7d6998a2069aedecd42818168ee0035b45fb7
itochs <halglobe0108@gmail.com>
2023-11-21 03:19:42 +0900

    定数の切り出し

--- server/app/app.py
+++ server/app/app.py
@@ -14,16 +14,6 @@ from route.projects.songs.music_parts.measure import part_measure
 from route.projects.songs.music_parts.measure.music_loop import mesure_music_loop
 from route.topic import topic
 
-fix_len = 4
-topic_n = 4
-excitement_len = 32
-selected_constitution_determine = 1
-selected_fix_determine = 0
-
-PARTS = ["Drums", "Bass", "Synth", "Sequence"]
-part_name2index = {"Drums": 0, "Bass": 1, "Synth": 2, "Sequence": 3}
-
-
 cred = credentials.Certificate("./credentials.json")
 firebase_app = firebase_admin.initialize_app(cred)
 
