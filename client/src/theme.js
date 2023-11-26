059acbb1cc5646950a58794e4127b25386db9bca
itochs <halglobe0108@gmail.com>
2023-11-26 14:57:19 +0900

    明るいのと暗いののテーマ

--- client/src/theme.js
+++ client/src/theme.js
@@ -3,10 +3,18 @@ import { extendTheme, theme } from "@chakra-ui/react";
 const extendedTheme = extendTheme({
   colors: {
     part: {
-      drums: theme.colors.red[200],
-      bass: theme.colors.yellow[200],
-      synth: theme.colors.green[200],
-      sequence: theme.colors.blue[200],
+      light: {
+        drums: theme.colors.red[200],
+        bass: theme.colors.yellow[200],
+        synth: theme.colors.green[200],
+        sequence: theme.colors.blue[200],
+      },
+      dark: {
+        drums: theme.colors.red[500],
+        bass: theme.colors.yellow[500],
+        synth: theme.colors.green[500],
+        sequence: theme.colors.blue[500],
+      },
     },
   },
 });
