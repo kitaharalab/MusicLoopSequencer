9ea2b63a2a1fb9dfdf8742eaaf06f4c421997d40
itochs <halglobe0108@gmail.com>
2023-11-29 01:46:07 +0900

    vscodeをclientで開いたのでaliasの再設定

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
-    alias: [{ find: "~", replacement: path.resolve(__dirname, "src") }],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
   plugins: [react()],
 });
