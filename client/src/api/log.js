d9e773c5b4d21c925bbe12248fd6f75f99e469ec
itochs <halglobe0108@gmail.com>
2023-11-29 21:24:54 +0900

    audio log

---
+++ client/src/api/log.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+import { auth } from "./authentication/firebase";
+
+export async function sendSongPlayLog(projectId, songId) {
+  const url = `${
+    import.meta.env.VITE_SERVER_URL
+  }/projects/${projectId}/songs/${songId}/wav`;
+  const idToken = await auth.currentUser?.getIdToken();
+  axios.post(
+    url,
+    { play: true },
+    { headers: { Authorization: `Bearer ${idToken}` } },
+  );
+}
+
+export async function sendSongPauseLog(projectId, songId) {
+  const url = `${
+    import.meta.env.VITE_SERVER_URL
+  }/projects/${projectId}/songs/${songId}/wav`;
+  const idToken = await auth.currentUser?.getIdToken();
+  axios.post(
+    url,
+    { pause: true },
+    { headers: { Authorization: `Bearer ${idToken}` } },
+  );
+}
+
+export async function sendSongStopLog(projectId, songId) {
+  const url = `${
+    import.meta.env.VITE_SERVER_URL
+  }/projects/${projectId}/songs/${songId}/wav`;
+  const idToken = await auth.currentUser?.getIdToken();
+  axios.post(
+    url,
+    { stop: true },
+    { headers: { Authorization: `Bearer ${idToken}` } },
+  );
+}
