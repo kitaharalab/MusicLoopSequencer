0df96573489e3ebcaa43bb329837485a13d8a55c
itochs <halglobe0108@gmail.com>
2023-12-01 05:01:52 +0900

    使っていないsliceの削除

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,23 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import apiParamSlice from "./apiParamSlice";
-import blockCanvasSlice from "./blockCanvasSlice";
 import linesSlice from "./linesSlice";
 import musicDataSlice from "./musicDataSlice";
-import musicLoopSlice from "./musicLoopSlice";
-import songIdSlice from "./songIdSlice";
-import soundDataSlice from "./soundDataSlice";
 import soundsSlice from "./soundsSlice";
 
 export const store = configureStore({
   reducer: {
     lines1: linesSlice,
     sounds: soundsSlice,
-    canvas: blockCanvasSlice,
     musicData: musicDataSlice,
-    songId: songIdSlice,
-    musicLoop: musicLoopSlice,
-    soundData: soundDataSlice,
     apiParams: apiParamSlice,
   },
 });
