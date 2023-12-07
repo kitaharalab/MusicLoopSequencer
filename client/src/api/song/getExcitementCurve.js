9a02a4ee5c99a6848accbb01997edd86ca4a761b
itochs <halglobe0108@gmail.com>
2023-12-07 19:04:12 +0900

    盛り上がり度曲線の取得

---
+++ client/src/api/song/getExcitementCurve.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+import { auth } from "../authentication/firebase";
+
+export default async function getExcitementCurve(projectId, songId) {
+  // "/projects/<int:projectid>/songs/<int:songid>"
+  if (projectId == null || songId == null) {
+    return Promise.reject(new Error("projectId or songId is null"));
+  }
+  const url = new URL(
+    `${import.meta.env.VITE_SERVER_URL}/projects/${projectId}/songs/${songId}`,
+  );
+
+  const idToken = await auth.currentUser?.getIdToken();
+  const response = await axios.get(url, {
+    headers: {
+      Authorization: `Bearer ${idToken}`,
+    },
+  });
+  const { data } = response;
+  const {
+    excitement_curve: { curve, max_value: max },
+  } = data;
+
+  return { curve, max };
+}
