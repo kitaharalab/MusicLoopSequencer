1d23e61ec0e0650b319663ed15889ef0a25d3fb1
itochs <halglobe0108@gmail.com>
2023-12-07 19:58:07 +0900

    プリセットの取得

--- client/src/api/song/getExcitementCurve.js
+++ client/src/api/song/getExcitementCurve.js
@@ -4,9 +4,24 @@ import { auth } from "../authentication/firebase";
 
 export default async function getExcitementCurve(projectId, songId) {
   // "/projects/<int:projectid>/songs/<int:songid>"
-  if (projectId == null || songId == null) {
+  if (projectId == null) {
     return Promise.reject(new Error("projectId or songId is null"));
   }
+
+  if (songId == null) {
+    const url = new URL(
+      `${import.meta.env.VITE_SERVER_URL}/projects/${projectId}/songs/preset`,
+    );
+    const idToken = await auth.currentUser?.getIdToken();
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${idToken}`,
+      },
+    });
+    const { data } = response;
+    return data;
+  }
+
   const url = new URL(
     `${import.meta.env.VITE_SERVER_URL}/projects/${projectId}/songs/${songId}`,
   );
