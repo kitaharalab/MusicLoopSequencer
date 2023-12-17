027d8117d300529bd7565bb08a8a06bdda6fc773
itochs <halglobe0108@gmail.com>
2023-12-17 23:47:07 +0900

    cleanはテーブルがあることを前提にしている気がする

--- server/sql/dump.sh
+++ server/sql/dump.sh
@@ -1,2 +1,2 @@
-pg_dump --no-owner --no-privileges --schema-only --clean > dump_schema.sql
-pg_dump --no-owner --no-privileges -t parts -t loops -t loop_topics -t topics --data-only --clean > dump.sql
+pg_dump --no-owner --no-privileges --schema-only > dump_schema.sql
+pg_dump --no-owner --no-privileges -t parts -t loops -t loop_topics -t topics --data-only > dump.sql
