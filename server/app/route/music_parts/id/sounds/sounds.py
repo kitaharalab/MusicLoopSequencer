1d2153271ff84f5303d865314d650354045ffe44
itochs <halglobe0108@gmail.com>
2023-12-20 11:03:11 +0900

    memoize

--- server/app/route/music_parts/id/sounds/sounds.py
+++ server/app/route/music_parts/id/sounds/sounds.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, jsonify, make_response
 from sqls import get_loop_positions_by_part
+from cache import cache
 
 sounds = Blueprint("sounds", __name__)
 
 
 @sounds.route("/parts/<int:partid>/sounds", methods=["GET"])
+@cache.memoize()
 def get_infomation_of_sounds(partid):
     response = get_loop_positions_by_part(partid)
     return make_response(jsonify(response))
