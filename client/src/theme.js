49b64f3f2defbd70fe4700b3dd91072285549eae
itochs <halglobe0108@gmail.com>
2023-12-24 21:59:25 +0900

    テーマカラー変更，色の順序を手動作成

--- client/src/theme.js
+++ client/src/theme.js
@@ -4,16 +4,22 @@ const extendedTheme = extendTheme({
   colors: {
     part: {
       light: {
-        drums: theme.colors.red[100],
-        bass: theme.colors.yellow[100],
-        synth: theme.colors.green[100],
-        sequence: theme.colors.blue[100],
+        drums: theme.colors.red[200],
+        bass: theme.colors.yellow[200],
+        synth: theme.colors.purple[200],
+        sequence: theme.colors.cyan[200],
       },
       dark: {
         drums: theme.colors.red[500],
         bass: theme.colors.yellow[500],
-        synth: theme.colors.green[500],
-        sequence: theme.colors.blue[500],
+        synth: theme.colors.purple[500],
+        sequence: theme.colors.cyan[500],
+      },
+      sequence: {
+        drums: ["#320E0E", "#802323", "#E53E3E", "#FF9999", "#FFD9D9"],
+        bass: ["#33260B", "#805E1B", "#D69E2E", "#FFDD99", "#FFF2D9"],
+        synth: ["#100D1A", "#654D99", "#AF91F2", "#D8CEF2", "#F6F2FF"],
+        sequence: ["#002B33", "#3A7580", "#70BDCC", "#A1DBE6", "#D9F9FF"],
       },
     },
   },
