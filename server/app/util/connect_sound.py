69f70a55b253d52b9895b3b2417a055b3c469d52
itochs <halglobe0108@gmail.com>
2023-12-24 13:14:02 +0900

    mp3にしても変わらない気がした

--- server/app/util/connect_sound.py
+++ server/app/util/connect_sound.py
@@ -20,7 +20,7 @@ def connect_sound(sound_list_by_measure_part, project_id, mode, song_id):
     wav_data = io.BytesIO()
     # 2Byte = 16bit
     output_sound.set_sample_width(2).set_frame_rate(44100).export(
-        wav_data, format="mp3"
+        wav_data, format="wav"
     )
     wav_data_bytes = wav_data.getvalue()
     return song_id, wav_data_bytes
@@ -48,5 +48,5 @@ def wav_overlay(loop_wav_by_part: list):
 @cache.memoize()
 def wav_to_audio_segment(wav_data):
     wav_data = io.BytesIO(wav_data)
-    sound = AudioSegment.from_file(wav_data, format="mp3")
+    sound = AudioSegment.from_file(wav_data, format="wav")
     return sound
