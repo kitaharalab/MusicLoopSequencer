dc70f46ba24a8fd4d8aa2ce79e36ced0e5f91817
itochs <halglobe0108@gmail.com>
2023-12-31 19:06:59 +0900

    rename table cloumn

--- server/app/sqls/user.py
+++ server/app/sqls/user.py
@@ -3,16 +3,16 @@ from psycopg2.extras import DictCursor
 from .connection import get_connection
 
 
-def add_user(user_id: str):
-    create_sql = "INSERT INTO users (user_id) VALUES (%s)"
+def add_user(user_id: str, own_id):
+    create_sql = "INSERT INTO users (user_id, lancers_id) VALUES (%s, %s)"
     with get_connection() as conn:
         with conn.cursor() as cur:
-            cur.execute(create_sql, (user_id,))
+            cur.execute(create_sql, (user_id, own_id))
             conn.commit()
 
 
 def get_user(user_id: str):
-    select_sql = "SELECT * FROM users WHERE user_id = %s"
+    select_sql = "SELECT * FROM users WHERE firebase_id = %s"
     with get_connection() as conn:
         with conn.cursor(cursor_factory=DictCursor) as cur:
             cur.execute(select_sql, (user_id,))
