2bb7cca14e8deca8defcc12e8d0cf614828b8950
itochs <halglobe0108@gmail.com>
2023-12-31 22:58:08 +0900

    ユーザー登録を認証から切り離す

--- server/app/sqls/user.py
+++ server/app/sqls/user.py
@@ -1,6 +1,9 @@
 from psycopg2.extras import DictCursor
 
+from verify import get_user_own_id
 from .connection import get_connection
+
+from .topic import get_topic_preferences, add_topic_preferences
 
 
 def add_user(user_id: str, own_id):
@@ -18,3 +21,14 @@ def get_user(user_id: str):
             cur.execute(select_sql, (user_id,))
             result = cur.fetchone()
             return dict(result) if result is not None else None
+
+
+def register_user(user_id: str, user_own_id):
+    exist_user = get_user(user_id) is not None
+    if not exist_user:
+        own_id = get_user_own_id()
+        add_user(user_id, own_id)
+
+    exist_topic_preferences = get_topic_preferences(user_id) is not None
+    if not exist_topic_preferences:
+        add_topic_preferences(user_id)
