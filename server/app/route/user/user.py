95a3ebbcf27b93ca462a0ba729bbe87198d7f080
itochs <halglobe0108@gmail.com>
2023-12-31 23:32:47 +0900

    余計なものを削除

--- server/app/route/user/user.py
+++ server/app/route/user/user.py
@@ -11,6 +11,6 @@ def add_user(user_id):
     params = request.get_json()
     user_own_id = params.get("own_id")
     if user_own_id is None:
-        raise AuthError("Missing user own id")(user_id, user_own_id)
+        raise AuthError("Missing user own id")
 
     register_user(user_id, user_own_id)
