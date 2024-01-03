4225648ac3eb8d8fe70a8ae2acea0f642c680a58
itochs <halglobe0108@gmail.com>
2024-01-03 16:11:59 +0900

    signinとregisterを別にする

--- server/app/route/user/user.py
+++ server/app/route/user/user.py
@@ -1,17 +1,53 @@
-from flask import Blueprint, request
-from sqls import register_user
+from flask import Blueprint, request, jsonify
 from verify import require_auth
+from .signin import check_sign_in
+from .register import register_user
 
 user = Blueprint("user", __name__)
 
 
-@user.route("/user", methods=["POST"])
+@user.route("/user/signin", methods=["POST"])
 @require_auth
-def add_user(user_id):
+def sign_in(user_id):
+    # firebase側では既にログインされているので，ここではIDのチェックと登録を主にする
     params = request.get_json()
     user_own_id = params.get("own_id")
     if user_own_id is None:
-        return "Missing user own id", 401
+        return jsonify(
+            {
+                "status": False,
+            }
+        ), 401
 
+    if check_sign_in(user_id, user_own_id):
+        return jsonify(
+            {
+                "status": True,
+            }
+        ), 200
+
+    return jsonify(
+        {
+            "status": False,
+        }
+    ), 401
+
+
+@user.route("/user/register", methods=["POST"])
+@require_auth
+def register(user_id):
+    params = request.get_json()
+    user_own_id = params.get("own_id")
     register_status = register_user(user_id, user_own_id)
-    return {"result": register_status}, 200
+    if register_status:
+        return jsonify(
+            {
+                "status": register_status,
+            }
+        ), 200
+
+    return jsonify(
+        {
+            "status": register_status,
+        }
+    ), 401
