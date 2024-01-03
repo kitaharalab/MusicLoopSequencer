0ebdd6a40a4bc9e8814b9fb45f0a9dc96d61336e
itochs <halglobe0108@gmail.com>
2024-01-03 13:41:18 +0900

    userが存在するときにIDを設定

--- client/src/components/signIn/SignIn.jsx
+++ client/src/components/signIn/SignIn.jsx
@@ -8,24 +8,28 @@ import {
   Input,
   Stack,
 } from "@chakra-ui/react";
-import { signIn, useUser } from "../Auth";
+import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
 
-function SignInUI() {
+import { signIn, useUser } from "../Auth";
+
+import registerUser from "@/api/registerUser";
+
+function SignInUI({ handleSignIn }) {
   return (
     <Container>
       <Stack spacing={8}>
         <Box textAlign="center" marginTop={8}>
-          <Heading size={"sm"}>IDを入力してログイン</Heading>
+          <Heading size="sm">IDを入力してログイン</Heading>
         </Box>
         <Box>
           <form
             onSubmit={(event) => {
               event.preventDefault();
               const inputIdEl = event.target.elements.id;
-              console.log(inputIdEl.value);
+              const userOwnId = inputIdEl.value;
+              handleSignIn(userOwnId);
               inputIdEl.value = "";
-              signIn();
             }}
           >
             <Stack spacing="6">
@@ -45,5 +49,19 @@ function SignInUI() {
 
 export default function SignIn() {
   const user = useUser();
-  return user ? <Navigate to={"/"} /> : <SignInUI />;
+  const [userOwnId, setUserOwnId] = useState("");
+
+  if (user) {
+    registerUser(userOwnId);
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <SignInUI
+      handleSignIn={(inputUserOwnId) => {
+        setUserOwnId(inputUserOwnId);
+        signIn();
+      }}
+    />
+  );
 }
