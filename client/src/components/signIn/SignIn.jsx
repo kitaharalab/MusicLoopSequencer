281323cfd759630d90fd0792c7bc2d92c19a07d9
itochs <halglobe0108@gmail.com>
2024-01-03 21:31:35 +0900

    サインインできるかチェックと，ユーザーの登録

--- client/src/components/signIn/SignIn.jsx
+++ client/src/components/signIn/SignIn.jsx
@@ -1,21 +1,25 @@
 import {
   Box,
   Button,
+  Checkbox,
   Container,
   FormControl,
+  FormErrorMessage,
+  FormHelperText,
   FormLabel,
   Heading,
   Input,
   Stack,
 } from "@chakra-ui/react";
-import React, { useState } from "react";
-import { Navigate } from "react-router-dom";
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-import { signIn, useUser } from "../Auth";
+import { signIn as firebaseSignIn, signOut, useUser } from "../Auth";
 
+import checkSignIn from "@/api/authentication/checkSignIn";
 import registerUser from "@/api/authentication/registerUser";
 
-function SignInUI({ handleSignIn }) {
+function SignInUI({ handleSignIn, isError }) {
   return (
     <Container>
       <Stack spacing={8}>
@@ -23,24 +27,41 @@ function SignInUI({ handleSignIn }) {
           <Heading size="sm">IDを入力してログイン</Heading>
         </Box>
         <Box>
-          <form
-            onSubmit={(event) => {
-              event.preventDefault();
-              const inputIdEl = event.target.elements.id;
-              const userOwnId = inputIdEl.value;
-              handleSignIn(userOwnId);
-              inputIdEl.value = "";
-            }}
-          >
-            <Stack spacing="6">
-              <FormControl isRequired>
-                <FormLabel>ID</FormLabel>
-                <Input type="text" name="id" />
-              </FormControl>
+          <FormControl isInvalid={isError}>
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+                const { elements } = event.target;
+                const userOwnId = elements.id.value;
+                const wantRegister = elements.register.checked;
+                handleSignIn(userOwnId, wantRegister);
 
-              <Button type="submit">Sign in with Google</Button>
-            </Stack>
-          </form>
+                elements.id.value = "";
+              }}
+            >
+              <Stack spacing="6">
+                <FormControl isRequired>
+                  <FormLabel>ID</FormLabel>
+                  <Input type="text" name="id" />
+                </FormControl>
+                <FormControl isInvalid={isError}>
+                  <Checkbox name="register">IDを登録する</Checkbox>
+                  <FormHelperText>
+                    以前にIDを登録したことがある場合は上書きされます
+                  </FormHelperText>
+                  <FormErrorMessage>
+                    IDを忘れた場合、このボタンをクリックして再度ログインしてください。
+                  </FormErrorMessage>
+                </FormControl>
+
+                <Button type="submit">Sign in with Google</Button>
+              </Stack>
+            </form>
+
+            <FormErrorMessage>
+              ログインに失敗しました。GoogleアカウントやIDを確認してください
+            </FormErrorMessage>
+          </FormControl>
         </Box>
       </Stack>
     </Container>
@@ -50,18 +71,50 @@ function SignInUI({ handleSignIn }) {
 export default function SignIn() {
   const user = useUser();
   const [userOwnId, setUserOwnId] = useState("");
+  const [wantRegister, setWantRegister] = useState("");
+  const isError = useRef(false);
+  const navigate = useNavigate();
 
-  if (user) {
-    registerUser(userOwnId);
-    return <Navigate to="/" />;
+  async function signIn() {
+    const canSignIn = await checkSignIn(userOwnId);
+    if (canSignIn) {
+      navigate("/");
+    } else {
+      signOut();
+      isError.current = true;
+    }
   }
+
+  async function signInWithRegister() {
+    const registered = await registerUser(userOwnId);
+    if (registered) {
+      navigate("/");
+    } else {
+      signOut();
+      isError.current = true;
+    }
+  }
+
+  useEffect(() => {
+    if (!user || !userOwnId) {
+      return;
+    }
+
+    if (wantRegister) {
+      signInWithRegister();
+    } else {
+      signIn();
+    }
+  }, [user, userOwnId]);
 
   return (
     <SignInUI
-      handleSignIn={(inputUserOwnId) => {
+      handleSignIn={(inputUserOwnId, wantRegister) => {
         setUserOwnId(inputUserOwnId);
-        signIn();
+        setWantRegister(wantRegister);
+        firebaseSignIn();
       }}
+      isError={isError.current}
     />
   );
 }
