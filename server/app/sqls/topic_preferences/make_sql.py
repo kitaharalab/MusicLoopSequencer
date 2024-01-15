d043c77f1d666724cfc5abea6fe38752582d604d
itochs <halglobe0108@gmail.com>
2024-01-15 16:31:43 +0900

    DBに入っていないトピック選考度だけを入れるようにする

--- server/app/sqls/topic_preferences/make_sql.py
+++ server/app/sqls/topic_preferences/make_sql.py
@@ -1,10 +1,12 @@
-from sqls.topics import get_topic_with_excitement
+from sqls.topics import get_topic_with_excitement, get_topics
 
+from .get import get_topic_preferences_tuple
 from .util import get_default_topic_preferences
 
 
 def make_exists_topic_preferences(firebase_id: str):
-    topics = get_topic_with_excitement()
+    # topics = get_topic_with_excitement()
+    topics = get_topics()
     topic_ids = tuple(map(lambda x: x["id"], topics))
     sql = """
         SELECT
@@ -23,10 +25,23 @@ def make_exists_topic_preferences(firebase_id: str):
 
 def make_insert_topic_preferences(firebase_id: str):
     default_topic_preferences = get_default_topic_preferences(firebase_id)
-    print(default_topic_preferences)
+    current_topic_preferences = get_topic_preferences_tuple(firebase_id)
+
+    # DBに入っていないトピック選考度のデータを探す
+    insert_topic_preferences = []
+    if current_topic_preferences:
+        no_value_current_topic_preferences = set(
+            [ctp[:-1] for ctp in current_topic_preferences]
+        )
+        for tp in default_topic_preferences:
+            if tp[:-1] not in no_value_current_topic_preferences:
+                insert_topic_preferences.append(tp)
+    else:
+        insert_topic_preferences = default_topic_preferences
+
     sql = """
         INSERT INTO
             topic_preferences (user_id, part_id, excitement, topic_id, VALUE)
         VALUES %s
     """
-    return (sql, default_topic_preferences)
+    return (sql, insert_topic_preferences)
